7f42bb40836cc6fb7dd28b0024e953e1b384aaa3
ibmdev <ibmdev102017@gmail.com>
2019-06-08 20:20:01 +0200

    Release build-info

--- bets-odds-unibet-ng/src/environments/build_info.ts
+++ bets-odds-unibet-ng/src/environments/build_info.ts
@@ -1,7 +1,7 @@
 
 export const versions = {
-   version: '4.0.14',
-   revision: 'eea830ef',
+   version: '4.0.15',
+   revision: '065a1d17',
    branch: 'master'
    // tslint:disable-next-line:eofline
  };
